554d3941ebf4e148ae2399c13aa44e1d2a301ba4
Wim R. Crols <wim.crols@xenit.eu>
2019-04-15 10:03:40 +0200

    ALFREDAPI-354: Add Alfresco 6.0 specific Java code

---
+++ apix-impl/60/src/main/java/eu/xenit/apix/alfresco60/SearchFacetsServiceImpl60.java
@@ -0,0 +1,21 @@
+package eu.xenit.apix.alfresco60;
+
+import eu.xenit.apix.alfresco.search.SearchFacetsServiceImpl5x;
+import eu.xenit.apix.translation.ITranslationService;
+import org.alfresco.repo.search.impl.solr.facet.SolrFacetService;
+import org.alfresco.service.ServiceRegistry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+public class SearchFacetsServiceImpl60 extends SearchFacetsServiceImpl5x {
+
+    private static final Logger logger = LoggerFactory.getLogger(SearchFacetsServiceImpl60.class);
+
+    @Autowired
+    public SearchFacetsServiceImpl60(ServiceRegistry serviceRegistry, SolrFacetService facetService,
+            ITranslationService translationService) {
+        super(serviceRegistry, facetService, translationService);
+    }
+}
---
+++ apix-impl/60/src/main/java/eu/xenit/apix/alfresco60/SpringConfiguration.java
@@ -0,0 +1,48 @@
+package eu.xenit.apix.alfresco60;
+
+import eu.xenit.apix.alfresco.ApixToAlfrescoConversion;
+import eu.xenit.apix.alfresco.search.SearchFacetsService;
+import eu.xenit.apix.alfresco.search.SearchResultCountService;
+import eu.xenit.apix.translation.ITranslationService;
+import org.alfresco.repo.i18n.MessageService;
+import org.alfresco.repo.search.impl.solr.facet.SolrFacetService;
+import org.alfresco.service.ServiceRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SpringConfiguration {
+
+    @Autowired
+    ServiceRegistry serviceRegistry;
+
+    @Autowired
+    SolrFacetService solrFacetService;
+
+    @Autowired
+    ApixToAlfrescoConversion apixToAlfrescoConversion;
+
+    @Autowired
+    @Qualifier("messageService")
+    private MessageService messageService;
+
+    @Autowired
+    private ITranslationService translationService;
+
+    public SearchFacetsService eu_xenit_apix_search_searchFacetsService60Apix() {
+        return new SearchFacetsServiceImpl60(serviceRegistry, solrFacetService, translationService);
+    }
+
+    @Bean(name = "eu.xenit.apix.search.SearchFacetsService60")
+    public SearchFacetsService searchFacetsServiceApix() {
+        return new SearchFacetsServiceImpl60(serviceRegistry, solrFacetService, translationService);
+    }
+
+    @Bean(name = "eu.xenit.apix.search.SearchResultCountService60")
+    public SearchResultCountService searchResultCountService() {
+        return new SearchResultCountServiceImpl60();
+    }
+}
