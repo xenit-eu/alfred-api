148fc5d8747991c162a6cf5e7452ce15c60703bd
Wim R. Crols <wim.crols@xenit.eu>
2019-05-13 17:18:32 +0200

    ALFREDAPI-373: Generalize PropertyServiceImpl

--- apix-impl/src/main/java/eu/xenit/apix/alfresco/properties/PropertyServiceImpl.java
+++ apix-impl/src/main/java/eu/xenit/apix/alfresco/properties/PropertyServiceImpl.java
@@ -1,31 +1,52 @@
 package eu.xenit.apix.alfresco.properties;
 
+import com.github.dynamicextensionsalfresco.osgi.OsgiService;
 import eu.xenit.apix.alfresco.ApixToAlfrescoConversion;
 import eu.xenit.apix.dictionary.properties.IPropertyService;
 import eu.xenit.apix.properties.PropertyConstraintDefinition;
+import eu.xenit.apix.properties.PropertyFacetable;
 import eu.xenit.apix.properties.PropertyIndexOptions;
 import eu.xenit.apix.properties.PropertyTokenised;
 import java.util.ArrayList;
 import java.util.List;
+import org.alfresco.repo.dictionary.Facetable;
 import org.alfresco.repo.dictionary.IndexTokenisationMode;
 import org.alfresco.service.cmr.dictionary.ConstraintDefinition;
 import org.alfresco.service.cmr.dictionary.DictionaryService;
 import org.alfresco.service.cmr.dictionary.PropertyDefinition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
 
 
-public abstract class PropertyServiceImpl implements IPropertyService {
+@OsgiService
+@Component("eu.xenit.apix.properties.IPropertyService")
+@Primary
+public class PropertyServiceImpl implements IPropertyService {
 
     private static final Logger logger = LoggerFactory.getLogger(PropertyServiceImpl.class);
     protected DictionaryService dictionaryService;
     protected ApixToAlfrescoConversion c;
 
+    @Autowired
     public PropertyServiceImpl(DictionaryService dictionaryService, ApixToAlfrescoConversion c) {
         this.dictionaryService = dictionaryService;
         this.c = c;
     }
 
+    public PropertyIndexOptions GetPropertyIndexOptions(PropertyDefinition definition) {
+        if (!definition.isIndexed()) {
+            return null;
+        }
+        PropertyIndexOptions IndexOptions = new PropertyIndexOptions();
+        IndexOptions.setFacetable(FacetableFromApix(definition.getFacetable()));
+        IndexOptions.setStored(definition.isStoredInIndex());
+        IndexTokenisationMode tokenisationMode = definition.getIndexTokenisationMode();
+        IndexOptions.setTokenised(TokenisedFromApix(tokenisationMode));
+        return IndexOptions;
+    }
 
     public eu.xenit.apix.properties.PropertyDefinition GetPropertyDefinition(eu.xenit.apix.data.QName qname) {
         if (!IsValidPropertyQName(qname)) {
@@ -49,19 +70,6 @@ public abstract class PropertyServiceImpl implements IPropertyService {
         return propertyDefinitionUnderConstruction;
     }
 
-    private List<PropertyConstraintDefinition> GetConstraints(PropertyDefinition definition) {
-        List<PropertyConstraintDefinition> constraintsUnderConstruction = new ArrayList<>();
-        for (ConstraintDefinition constraint : definition.getConstraints()) {
-            PropertyConstraintDefinition constraintDefinition = new PropertyConstraintDefinition();
-            constraintDefinition.setConstraintType(constraint.getConstraint().getType());
-            constraintDefinition.setParameters(constraint.getConstraint().getParameters());
-            constraintsUnderConstruction.add(constraintDefinition);
-        }
-        return constraintsUnderConstruction;
-    }
-
-    public abstract PropertyIndexOptions GetPropertyIndexOptions(PropertyDefinition definition);
-
     public boolean IsValidPropertyQName(eu.xenit.apix.data.QName qname) {
         try {
             if (!c.HasAlfrescoQname(qname)) {
@@ -76,7 +84,29 @@ public abstract class PropertyServiceImpl implements IPropertyService {
         }
     }
 
-    protected PropertyTokenised TokenisedFromApix(IndexTokenisationMode tokMode) {
+    private List<PropertyConstraintDefinition> GetConstraints(PropertyDefinition definition) {
+        List<PropertyConstraintDefinition> constraintsUnderConstruction = new ArrayList<>();
+        for (ConstraintDefinition constraint : definition.getConstraints()) {
+            PropertyConstraintDefinition constraintDefinition = new PropertyConstraintDefinition();
+            constraintDefinition.setConstraintType(constraint.getConstraint().getType());
+            constraintDefinition.setParameters(constraint.getConstraint().getParameters());
+            constraintsUnderConstruction.add(constraintDefinition);
+        }
+        return constraintsUnderConstruction;
+    }
+
+    private static PropertyFacetable FacetableFromApix(Facetable facetable) {
+        switch (facetable) {
+            case FALSE:
+                return PropertyFacetable.FALSE.FALSE;
+            case TRUE:
+                return PropertyFacetable.TRUE;
+            default:
+                return PropertyFacetable.DEFAULT;
+        }
+    }
+
+    private PropertyTokenised TokenisedFromApix(IndexTokenisationMode tokMode) {
         switch (tokMode) {
             case FALSE:
                 return PropertyTokenised.FALSE;
@@ -87,4 +117,5 @@ public abstract class PropertyServiceImpl implements IPropertyService {
         }
         throw new Error("Unhandled type");
     }
+
 }
