0b68aff5372ae3ebb8c7213a24c84dd92e1b01ef
Robrecht Vanhuysse <robrecht.vanhuysse@xenit.eu>
2019-06-20 10:36:00 +0200

    ALFREDAPI-372 Prevent long highlight queries

--- apix-impl/52/src/main/java/eu/xenit/apix/alfresco52/SearchServiceImpl52.java
+++ apix-impl/52/src/main/java/eu/xenit/apix/alfresco52/SearchServiceImpl52.java
@@ -38,6 +38,29 @@ public class SearchServiceImpl52 extends SearchService {
         // Build parameters normally
         SearchParameters parameters = super.buildSearchParameters(postQuery);
 
+        /*
+        * Dont set highlights if there is no searchTerm and the query length surpasses the cap.
+        * The cap on the querylength is set at 600 characters to have a generous margin on the query, but also maintain
+        * a reasonable margin for other query parameters.
+        *
+        * We do this because the alfresco/solr implementation of highlights, which takes the searchTerms or search query
+        * and adds it in the querystring of the request between alfresco and solr.
+        * This breaks when:
+        * There are no searchTerms in the query; Alfresco does not set these, and apix does not yet support these.
+        * AND
+        * The query is too long; if there are no searchTerms given, alfresco will instead use the full search query to
+        * add to the querystring. For long queries this might violate the url parameter length restriction (around 2000
+        *  chars. See appropriate RFC for exact number.) and trigger a 400 response in the solr.
+        *
+        * This might be fixable by learning how the searchTerms are supposed to work (but these are largely undocumented.
+        * Best lead: solr docs on hl.q) and supplying these.
+        * Or we could suggest to alfresco (and solr?) development to pass these params in the request body.
+        * */
+        String searchTerm = parameters.getSearchTerm();
+        if ((searchTerm == null || searchTerm.isEmpty()) && parameters.getQuery().length() > 600) {
+            return parameters;
+        }
+
         // Extend the parameters with highlights, if they were defined + convert them to Alfresco's internal format
         if (postQuery.getHighlight() != null) {
             HighlightOptions options = postQuery.getHighlight();
