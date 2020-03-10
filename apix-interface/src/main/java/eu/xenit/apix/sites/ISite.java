53d40745639f0fc85550514a90ffc8e262dfca1a
Kenneth Piot <kenneth.piot@xenit.eu>
2020-03-10 16:42:27 +0100

    ALFREDAPI-417 Added webscript for retrieving available sites + webscript for retrieving recursive parents of a node
    HA-29 WIP
    
    HA-29 WIP

---
+++ apix-interface/src/main/java/eu/xenit/apix/sites/ISite.java
@@ -0,0 +1,13 @@
+package eu.xenit.apix.sites;
+
+import eu.xenit.apix.data.NodeRef;
+import java.util.Map;
+
+public interface ISite {
+    NodeRef getNodeRef();
+    String getShortName();
+    String getTitle();
+    String getDescription();
+    boolean isPublic();
+    Map<String, NodeRef> getComponents();
+}
