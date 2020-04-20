fa13438beac917b23f02e45a689994bfaa62e8fe
Kenneth Piot <kenneth.piot@xenit.eu>
2020-04-20 14:29:37 +0200

    ALFREDAPI-417 Added AncestorsObject to allow for the correct output example in the Swagger documentation

---
+++ apix-rest-v1/src/main/java/eu/xenit/apix/rest/v1/nodes/AncestorsObject.java
@@ -0,0 +1,22 @@
+package eu.xenit.apix.rest.v1.nodes;
+
+import eu.xenit.apix.data.NodeRef;
+import java.util.List;
+
+public class AncestorsObject {
+    private NodeRef node;
+    private List<NodeRef> ancestors;
+
+    public NodeRef getNode() {
+        return node;
+    }
+
+    public List<NodeRef> getAncestors() {
+        return ancestors;
+    }
+
+    public AncestorsObject(NodeRef node, List<NodeRef> ancestors) {
+        this.node = node;
+        this.ancestors = ancestors;
+    }
+}
