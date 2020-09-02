2e90ff62f7cbcf508ee9bdb63b6f26d701bacab7
ranecdev <ranecdev@gmail.com>
2020-09-02 17:12:21 +0200

    ALFREDAPI-449 Change Comment API endpoints

--- apix-impl/src/integration-test/java/eu/xenit/apix/rest/v1/tests/CommentsTest.java
+++ apix-impl/src/integration-test/java/eu/xenit/apix/rest/v1/tests/CommentsTest.java
@@ -3,14 +3,28 @@ package eu.xenit.apix.rest.v1.tests;
 import static org.junit.Assert.assertEquals;
 
 import eu.xenit.apix.alfresco.ApixToAlfrescoConversion;
+import eu.xenit.apix.alfresco.metadata.NodeService;
+import eu.xenit.apix.comments.Comment;
+import eu.xenit.apix.comments.Conversation;
+import eu.xenit.apix.comments.ICommentService;
 import eu.xenit.apix.data.NodeRef;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Arrays;
 import java.util.HashMap;
 import org.alfresco.repo.forum.CommentService;
+import org.alfresco.repo.security.authentication.AuthenticationUtil;
+import org.alfresco.service.cmr.model.FileInfo;
+import org.apache.commons.httpclient.HttpStatus;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.fluent.Request;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,7 +45,13 @@ public class CommentsTest extends BaseTest {
     private CommentService alfrescoCommentService;
 
     @Autowired
+    private ICommentService commentService;
+
+    @Autowired
     private ApixToAlfrescoConversion apixConverter;
+
+    @Autowired
+    private NodeService nodeService;
 
     @Override
     public HashMap<String, NodeRef> init() {
@@ -44,7 +64,10 @@ public class CommentsTest extends BaseTest {
         org.alfresco.service.cmr.repository.NodeRef alfrescoCommentNode = alfrescoCommentService
                 .createComment(apixConverter.alfresco(initializedNodeRefs.get(TESTFILE_NAME)), commentTitle,
                         commentContent, false);
-        initializedNodeRefs.put(COMMENTNODE1, new NodeRef(alfrescoCommentNode.toString()));
+        Comment comment = commentService.addNewComment(initializedNodeRefs.get(TESTFILE_NAME), commentContent);
+        log.info("Created comment : " +  comment.getId());
+        log.info("Exists : " + nodeService.exists(comment.getId()));
+        initializedNodeRefs.put(COMMENTNODE1, comment.getId());
         org.alfresco.service.cmr.repository.NodeRef alfrescoCommentNode2 = alfrescoCommentService
                 .createComment(apixConverter.alfresco(initializedNodeRefs.get(NOUSERRIGHTS_FILE_NAME)), commentTitle,
                         commentContent, false);
@@ -53,19 +76,46 @@ public class CommentsTest extends BaseTest {
     }
 
     @Test
-    public void test_getComments() throws IOException, URISyntaxException {
+    public void testGetComments() throws IOException, URISyntaxException {
         HashMap<String, NodeRef> initializedRefs = init();
         String url = makeNodesUrl(initializedRefs.get(BaseTest.TESTFILE_NAME), "/comments", "admin", "admin");
         log.info("URL: {}", url);
         URIBuilder builder = new URIBuilder(url);
         builder.addParameter(PAGESIZE, "10");
         builder.addParameter(SKIPCOUNT,"0");
-        HttpResponse httpResponse = Request.Get(builder.build()).execute().returnResponse();
-        assertEquals(200, httpResponse.getStatusLine().getStatusCode());
+        //HttpResponse httpResponse = Request.Get(builder.build()).execute().returnResponse();
+        //assertEquals(200, httpResponse.getStatusLine().getStatusCode());
+
+        HttpGet req = new HttpGet(builder.build());
+        final CloseableHttpClient checkoutHttpclient = HttpClients.createDefault();
+        log.info("URL: {}", url);
+
+        try (CloseableHttpResponse response = checkoutHttpclient.execute(req)) {
+            String result = EntityUtils.toString(response.getEntity());
+            log.info("Result mutiple : " + result);
+            assertEquals(HttpStatus.SC_OK, response.getStatusLine().getStatusCode());
+        }
     }
 
     @Test
-    public void test_getComments_accessDenied() throws IOException, URISyntaxException {
+    public void testGetComment() throws IOException, URISyntaxException {
+        HashMap<String, NodeRef> initializedRefs = init();
+        String url = makeCommentsUrl(initializedRefs.get(COMMENTNODE1), "admin", "admin");
+        Conversation conversation = commentService.getComments(initializedRefs.get(COMMENTNODE1), 0, 10);
+        log.info("Comments : " + conversation.getComments() );
+        HttpGet req = new HttpGet(url);
+        final CloseableHttpClient checkoutHttpclient = HttpClients.createDefault();
+        log.info("URL: {}", url);
+
+        try (CloseableHttpResponse response = checkoutHttpclient.execute(req)) {
+            String result = EntityUtils.toString(response.getEntity());
+            log.info("Result : " + result);
+            assertEquals(HttpStatus.SC_OK, response.getStatusLine().getStatusCode());
+        }
+    }
+
+    @Test
+    public void testGetCommentsAccessDenied() throws IOException, URISyntaxException {
         HashMap<String, NodeRef> initializedRefs = init();
         String url = makeNodesUrl(initializedRefs.get(BaseTest.NOUSERRIGHTS_FILE_NAME), "/comments", BaseTest.USERWITHOUTRIGHTS,
                 BaseTest.USERWITHOUTRIGHTS);
@@ -78,7 +128,7 @@ public class CommentsTest extends BaseTest {
     }
 
     @Test
-    public void test_getComments_notFound() throws IOException, URISyntaxException {
+    public void testGetCommentsNotFound() throws IOException, URISyntaxException {
         HashMap<String, NodeRef> initializedRefs = init();
         String url = makeNodesUrl(new NodeRef("workspace://SpacesStore/00000000-0000-0000-0000-000000000000"), "/comments", "admin", "admin");
         log.info("URL: {}", url);
--- apix-rest-v1/src/main/java/eu/xenit/apix/rest/v1/nodes/NodesWebscript1.java
+++ apix-rest-v1/src/main/java/eu/xenit/apix/rest/v1/nodes/NodesWebscript1.java
@@ -672,6 +672,7 @@ public class NodesWebscript1 extends ApixV1Webscript {
                 Conversation comments = commentService.getComments(target, skipcount, pagesize);
                 boolean canCreate = permissionService.hasPermission(target, PermissionService.CREATE_CHILDREN);
                 comments.setCreatable(canCreate);
+                response.setStatus(HttpStatus.SC_OK);
                 writeJsonResponse(response, comments);
             } else {
                 throw new AccessDeniedException("User does not have permission to read parent node");
@@ -693,14 +694,39 @@ public class NodesWebscript1 extends ApixV1Webscript {
         final NodeRef target = new NodeRef(space, store, guid);
         if (nodeService.exists(target)) {
             Comment responseComment = commentService.addNewComment(target, newComment.getContent());
+            response.setStatus(HttpStatus.SC_OK);
             writeJsonResponse(response, responseComment);
         } else {
             writeNotFoundResponse(response, target);
         }
     }
 
-    @ApiOperation(value = "Updates a given comment on the given node.")
-    @Uri(value = "/nodes/{space}/{store}/{guid}/comment",
+    @ApiOperation(value = "Gets the comment on the given node.")
+    @Uri(value = "/comments/{space}/{store}/{guid}",
+            method = HttpMethod.GET)
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Success"),
+            @ApiResponse(code = 403, message = "Not Authorized"),
+            @ApiResponse(code = 404, message = "Not Found")
+    })
+    public void getComment(@UriVariable String space, @UriVariable String store, @UriVariable String guid,
+             WebScriptRequest request, WebScriptResponse response) throws IOException {
+        final NodeRef targetComment = new NodeRef(space, store, guid);
+        if (nodeService.exists(targetComment)) {
+            if(permissionService.hasPermission(targetComment, PermissionService.READ)) {
+                response.setStatus(HttpStatus.SC_OK);
+                writeJsonResponse(response, targetComment);
+            } else {
+                throw new AccessDeniedException(String.format("User does not have permission " +
+                        "to read the comment node %s", targetComment.toString()));
+            }
+        } else {
+            writeNotFoundResponse(response, targetComment);
+        }
+    }
+
+    @ApiOperation(value = "Updates the comment on the given node.")
+    @Uri(value = "/comments/{space}/{store}/{guid}",
             method = HttpMethod.PUT)
     @ApiResponses({
             @ApiResponse(code = 200, message = "Success"),
@@ -712,15 +738,15 @@ public class NodesWebscript1 extends ApixV1Webscript {
         final NodeRef targetComment = new NodeRef(space, store, guid);
         if (nodeService.exists(targetComment)) {
             Comment updatedComment = commentService.updateComment(targetComment, newComment.getContent());
-            response.setStatus(200);
+            response.setStatus(HttpStatus.SC_OK);
             writeJsonResponse(response, updatedComment);
         } else {
             writeNotFoundResponse(response, targetComment);
         }
     }
 
-    @ApiOperation(value = "Deletes a given comment on the given node.")
-    @Uri(value = "/nodes/{space}/{store}/{guid}/comment",
+    @ApiOperation(value = "Deletes the comment on the given node.")
+    @Uri(value = "/comments/{space}/{store}/{guid}",
             method = HttpMethod.DELETE)
     @ApiResponses({
             @ApiResponse(code = 200, message = "Success"),
@@ -732,8 +758,8 @@ public class NodesWebscript1 extends ApixV1Webscript {
         final NodeRef targetComment = new NodeRef(space, store, guid);
         if (nodeService.exists(targetComment)) {
             commentService.deleteComment(targetComment);
-            response.setStatus(200);
-            writeJsonResponse(response, "Comment deleted");
+            response.setStatus(HttpStatus.SC_OK);
+            writeJsonResponse(response, String.format("Comment %s deleted", targetComment.toString()));
         } else {
             writeNotFoundResponse(response, targetComment);
         }
