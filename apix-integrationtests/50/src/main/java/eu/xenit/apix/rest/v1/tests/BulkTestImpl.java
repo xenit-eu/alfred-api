b2b56769f7583d48477a596aff1c0c8c82ea5b8a
Robrecht Vanhuysse <robrecht.vanhuysse@xenit.eu>
2021-06-24 15:26:35 +0200

    ALFREDAPI-482 wrap JSONException in runtime ex for older versions to comply with throwing behaviour and method signature

--- apix-integrationtests/50/src/main/java/eu/xenit/apix/rest/v1/tests/BulkTestImpl.java
+++ apix-integrationtests/50/src/main/java/eu/xenit/apix/rest/v1/tests/BulkTestImpl.java
@@ -1,5 +1,6 @@
 package eu.xenit.apix.rest.v1.tests;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class BulkTestImpl extends BulkTest {
@@ -7,6 +8,10 @@ public class BulkTestImpl extends BulkTest {
 
     @Override
     public String jsonObjectGetStringFromInt(JSONObject targetObject, String key) {
-        return targetObject.getString(key);
+        try {
+            return targetObject.getString(key);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
