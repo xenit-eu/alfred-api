063aae9596f9e52c30283ea2ca45c896954226be
Robrecht Vanhuysse <robrecht.vanhuysse@xenit.eu>
2021-07-09 15:07:55 +0200

    ALFREDAPI-483 standardize input highlight test input instead of relying on default repo contents

--- apix-integrationtests/src/main/java/eu/xenit/apix/tests/search/TermHitHighlightingTest.java
+++ apix-integrationtests/src/main/java/eu/xenit/apix/tests/search/TermHitHighlightingTest.java
@@ -2,39 +2,73 @@ package eu.xenit.apix.tests.search;
 
 import static org.junit.Assert.assertEquals;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import eu.xenit.apix.alfresco.search.SearchService;
+import eu.xenit.apix.node.INodeService;
 import eu.xenit.apix.search.Highlights;
-import eu.xenit.apix.search.Highlights.HighlightResult;
 import eu.xenit.apix.search.QueryBuilder;
 import eu.xenit.apix.search.SearchQuery;
 import eu.xenit.apix.search.SearchQuery.HighlightOptions;
 import eu.xenit.apix.search.SearchQueryResult;
 import eu.xenit.apix.search.nodes.SearchSyntaxNode;
 import eu.xenit.apix.tests.BaseTest;
-import eu.xenit.apix.util.SolrTestHelper;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
-import java.util.List;
-import java.util.Map;
-import javax.sql.DataSource;
-import org.alfresco.repo.management.subsystems.SwitchableApplicationContextFactory;
+import org.alfresco.model.ContentModel;
+import org.alfresco.repo.transaction.RetryingTransactionHelper;
+import org.alfresco.service.cmr.model.FileInfo;
+import org.alfresco.service.cmr.repository.ContentData;
+import org.alfresco.service.cmr.repository.ContentWriter;
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.repository.NodeService;
+import org.apache.tika.io.IOUtils;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 
 public class TermHitHighlightingTest extends BaseTest {
 
     private static final Logger log = LoggerFactory.getLogger(TermHitHighlightingTest.class);
 
     @Autowired
+    INodeService nodeService;
+    @Autowired
     SearchService searchService;
+    @Autowired
+    RetryingTransactionHelper retryingTransactionHelper;
 
     private ObjectMapper mapper = new ObjectMapper();
+    private static final String FURIES_TXT =
+            "\nThe furies are at home\nin the mirror; it is their address.\nEven the clearest water,\nif deep enough can drown.\n"
+            + "\nNever think to surprise them.\nYour face approaching ever\nso friendly is the white flag\nthey ignore. There is no truce\n"
+            + "\nwith the furies. A mirror’s temperature\nis always at zero. It is ice\nin the veins. Its camera\nis an X-ray. It is a chalice\n"
+            + "\nheld out to you in\nsilent communion, where gaspingly\nyou partake of a shifting\nidentity never your own.";
+
+    @Before
+    public void createHighlightTestTXT() throws InterruptedException {
+        super.cleanUp();
+        retryingTransactionHelper.doInTransaction(() -> {
+            File txtFile = super.createTextFileWithContent("Furies.txt", FURIES_TXT);
+            FileInfo mainTestFolder = super.createMainTestFolder(getNodeAtPath("/app:company_home"));
+            InputStream inputStream = new FileInputStream(txtFile);
+            eu.xenit.apix.data.NodeRef target = nodeService.createNode(super.c.apix(mainTestFolder.getNodeRef()), txtFile.getName(),
+                    super.c.apix(ContentModel.TYPE_CONTENT));
+            nodeService.setContent(target, inputStream, txtFile.getName());
+            return null;
+        }, false, true);
+        solrHelper.waitForSolrSync();
+    }
+
+    @After
+    public void cleanupAfterHighlightTest() {
+        super.cleanUp();
+    }
 
     @Test
     /** Test all major parameters for term hit highlighting */
@@ -44,7 +78,7 @@ public class TermHitHighlightingTest extends BaseTest {
         SearchServiceTest.waitAWhile(20);
 
         URL expectedHighlightsJson = getClass().getClassLoader().getResource(
-                "highlight-response-jsons/searchRequestWithHighlighting_withAllMajorCustomParams.json");
+                "highlightTest/searchRequestWithHighlighting_withAllMajorCustomParams.json");
         Highlights expectedHighlightResult = mapper.readValue(expectedHighlightsJson, Highlights.class);
 
         /* The options set are equivalent to the following JSON:
@@ -52,7 +86,7 @@ public class TermHitHighlightingTest extends BaseTest {
                 "property": {
                     "exact": false,
                     "name": "cm:content",
-                    "value": "budget"
+                    "value": "furies"
                 }
             },
             "highlight": {
@@ -66,9 +100,9 @@ public class TermHitHighlightingTest extends BaseTest {
         */
         SearchQuery postQuery = new SearchQuery();
         QueryBuilder builder = new QueryBuilder();
-        SearchSyntaxNode rootNode = builder.property("cm:content", "budget").create();
+        SearchSyntaxNode rootNode = builder.property("cm:content", "furies").create();
         postQuery.setQuery(rootNode);
-        SearchQuery.HighlightOptions options = new HighlightOptions();
+        HighlightOptions options = new HighlightOptions();
         options.setSnippetCount(2);
         options.setMergeContiguous(Boolean.TRUE);
         options.setPrefix("!PREFIX!");
@@ -77,6 +111,12 @@ public class TermHitHighlightingTest extends BaseTest {
         postQuery.setHighlight(options);
 
         SearchQueryResult actualResult = searchService.query(postQuery);
-        assertEquals(expectedHighlightResult, actualResult.getHighlights());
+        // Splitting asserts into check on length and check on highlight content due to differing nodeId for expected
+        // and actual.
+        // We only expect this one result due to controlled input.
+        assertEquals(1, actualResult.getHighlights().getNoderefs().size());
+        // Given we expect only 1 entry, we can blindly pluck it from the map in the highlightResult
+        assertEquals(expectedHighlightResult.getNoderefs().entrySet().iterator().next().getValue(),
+                actualResult.getHighlights().getNoderefs().entrySet().iterator().next().getValue());
     }
 }
