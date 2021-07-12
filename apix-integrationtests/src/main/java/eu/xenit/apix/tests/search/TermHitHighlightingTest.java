cd810160dad08cb7f1be729120f2236544ecc515
Robrecht Vanhuysse <robrecht.vanhuysse@xenit.eu>
2021-07-12 09:07:43 +0200

    ALFREDAPI-483 replace txt resource with pdf for content indexing?

--- apix-integrationtests/src/main/java/eu/xenit/apix/tests/search/TermHitHighlightingTest.java
+++ apix-integrationtests/src/main/java/eu/xenit/apix/tests/search/TermHitHighlightingTest.java
@@ -2,37 +2,32 @@ package eu.xenit.apix.tests.search;
 
 import static org.junit.Assert.assertEquals;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import eu.xenit.apix.alfresco.search.SearchService;
 import eu.xenit.apix.search.Highlights;
-import eu.xenit.apix.search.Highlights.HighlightResult;
 import eu.xenit.apix.search.QueryBuilder;
 import eu.xenit.apix.search.SearchQuery;
 import eu.xenit.apix.search.SearchQuery.HighlightOptions;
 import eu.xenit.apix.search.SearchQueryResult;
 import eu.xenit.apix.search.nodes.SearchSyntaxNode;
 import eu.xenit.apix.tests.BaseTest;
-import eu.xenit.apix.util.SolrTestHelper;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
-import java.util.List;
-import java.util.Map;
-import javax.sql.DataSource;
 import org.alfresco.model.ContentModel;
-import org.alfresco.repo.management.subsystems.SwitchableApplicationContextFactory;
+import org.alfresco.repo.transaction.RetryingTransactionHelper;
 import org.alfresco.service.cmr.model.FileInfo;
 import org.alfresco.service.cmr.repository.ContentData;
 import org.alfresco.service.cmr.repository.ContentWriter;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
+import org.apache.tika.io.IOUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 
 public class TermHitHighlightingTest extends BaseTest {
 
@@ -40,22 +35,35 @@ public class TermHitHighlightingTest extends BaseTest {
 
     @Autowired
     SearchService searchService;
+    @Autowired
+    RetryingTransactionHelper retryingTransactionHelper;
 
     private ObjectMapper mapper = new ObjectMapper();
 
     @Before
-    public void createHighlightTestDocument() throws IOException {
-        NodeService nodeService = super.serviceRegistry.getNodeService();
-        FileInfo mainTestFolder = super.createMainTestFolder(getNodeAtPath("/app:company_home"));
-        FileInfo targetNodeFileInfo = super.createTestNode(mainTestFolder.getNodeRef(), "furies.txt");
-        NodeRef targetNode = targetNodeFileInfo.getNodeRef();
-        ContentWriter writer = super.serviceRegistry.getContentService()
-                .getWriter(targetNode, ContentModel.PROP_CONTENT, true);
-        writer.putContent(getClass().getClassLoader().getResource(
-                "highlightTest/furies.txt").openStream());
-        ContentData contentData = (ContentData) nodeService.getProperty(
-                targetNode, ContentModel.PROP_CONTENT);
-        nodeService.setProperty(targetNode, ContentModel.PROP_CONTENT, contentData);
+    public void createHighlightTestDocument() throws InterruptedException {
+        super.cleanUp();
+        retryingTransactionHelper.doInTransaction(() -> {
+            NodeService nodeService = super.serviceRegistry.getNodeService();
+            FileInfo mainTestFolder = super.createMainTestFolder(getNodeAtPath("/app:company_home"));
+            FileInfo targetNodeFileInfo = super.createTestNode(mainTestFolder.getNodeRef(), "furies.txt");
+            NodeRef targetNode = targetNodeFileInfo.getNodeRef();
+            ContentWriter writer = super.serviceRegistry.getContentService()
+                    .getWriter(targetNode, ContentModel.PROP_CONTENT, true);
+            writer.setMimetype("application/pdf");
+            InputStream resourceStream = getClass().getClassLoader().getResource(
+                    "highlightTest/Furies.pdf").openStream();
+            try {
+                writer.putContent(resourceStream);
+            } finally {
+                IOUtils.closeQuietly(resourceStream);
+            }
+            ContentData contentData = (ContentData) nodeService.getProperty(
+                    targetNode, ContentModel.PROP_CONTENT);
+            nodeService.setProperty(targetNode, ContentModel.PROP_CONTENT, contentData);
+            return null;
+        }, false, true);
+        solrHelper.waitForSolrSync();
     }
 
     @After
