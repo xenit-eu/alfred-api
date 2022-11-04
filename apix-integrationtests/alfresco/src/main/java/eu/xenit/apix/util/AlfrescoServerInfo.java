ce0dc450101b73cfc265f6c3e53bc40ce5388614
Zlatin Todorinski <zlatin.todorinski@xenit.eu>
2022-11-04 10:23:29 +0200

    ALFREDAPI-504 Cleanup

--- apix-integrationtests/alfresco/src/main/java/eu/xenit/apix/util/AlfrescoServerInfo.java
+++ apix-integrationtests/alfresco/src/main/java/eu/xenit/apix/util/AlfrescoServerInfo.java
@@ -6,25 +6,16 @@ import org.alfresco.service.descriptor.DescriptorService;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
 @Service
 @OsgiService
 public class AlfrescoServerInfo {
     // Part of Alfresco Private API
     private final DescriptorService descriptorService;
-    private final DataSource dataSource;
-    private NodeDAO nodeDAO;
+    private final NodeDAO nodeDAO;
 
     public AlfrescoServerInfo(@Qualifier("descriptorComponent") DescriptorService descriptorServiceParam,
-                              DataSource dataSourceParam,
                               NodeDAO nodeDAOParam) {
         descriptorService = descriptorServiceParam;
-        dataSource = dataSourceParam;
         nodeDAO = nodeDAOParam;
     }
 
