9ab11bf1698939deea30fc13777620e5a846dbcb
Zlatin Todorinski <zlatin.todorinski@xenit.eu>
2022-11-04 11:15:05 +0200

    ALFREDAPI-504 Correct max txn id lookup

--- apix-integrationtests/alfresco/src/main/java/eu/xenit/apix/util/AlfrescoServerInfo.java
+++ apix-integrationtests/alfresco/src/main/java/eu/xenit/apix/util/AlfrescoServerInfo.java
@@ -1,22 +1,27 @@
 package eu.xenit.apix.util;
 
 import com.github.dynamicextensionsalfresco.osgi.OsgiService;
-import org.alfresco.repo.domain.node.NodeDAO;
 import org.alfresco.service.descriptor.DescriptorService;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 @Service
 @OsgiService
 public class AlfrescoServerInfo {
     // Part of Alfresco Private API
     private final DescriptorService descriptorService;
-    private final NodeDAO nodeDAO;
+    private final DataSource dataSource;
 
     public AlfrescoServerInfo(@Qualifier("descriptorComponent") DescriptorService descriptorServiceParam,
-                              NodeDAO nodeDAOParam) {
+                              DataSource dataSourceParam) {
         descriptorService = descriptorServiceParam;
-        nodeDAO = nodeDAOParam;
+        dataSource = dataSourceParam;
     }
 
     public String getAlfrescoVersion() {
@@ -25,7 +30,22 @@ public class AlfrescoServerInfo {
                 .getVersion();
     }
 
+    /**
+     * Tried using SearchTrackingComponent and NodeDAO to getMaxTxnId, but they are not as consistent and quick
+     * as getting it from alf_node table.
+     */
     public long getAlfTransactionIdDAO() {
-        return nodeDAO.getMaxTxnId();
+        try (Connection connection = dataSource.getConnection()) {
+            try(final Statement stmt = connection.createStatement()) {
+                try(final ResultSet rs = stmt.executeQuery("select max( transaction_id ) from alf_node")) {
+                    if (rs.next()) {
+                        return rs.getInt(1);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return -1;
     }
 }
