29eef556f493fc832e9265e4655e0124cd635acd
Benjamin Van der Smissen <benjamin.vandersmissen@xenit.eu>
2024-07-15 14:44:19 +0200

    ALFREDAPI-548-split [Add] Tomcat context.xml file that initialises the dispatchServlet of Tomcat10 to allow MuliPartConfiguration and multiple forwadr slashes.     * [REMARK] This fix will only run in the Alfred-API docker-compose. This has to be implemented on the imported projects.     * context.xml file has to be copied to ${TOMCAT_HOME}/webapps/alfresco/META-INF/context.xml *  [Integration-test] VersionHistoryService has a weird apix issue.     * createVersion via the apix Implementation returns a license RO-issue. (Alfresco is not in RO-mode)     * Solved by changing apixVersionHistory to AlfrescoVersionHistory Bean...

--- apix-integrationtests/alfresco/src/main/java/eu/xenit/apix/tests/versionhistory/VersionHistoryServiceTestJavaApi.java
+++ apix-integrationtests/alfresco/src/main/java/eu/xenit/apix/tests/versionhistory/VersionHistoryServiceTestJavaApi.java
@@ -48,7 +48,8 @@ public class VersionHistoryServiceTestJavaApi extends JavaApiBaseTest {
     @Test
     public void TestGetVersionHistory() {
         Map<String, Serializable> versionProperties = new HashMap<>();
-
+        logger.error("versionHistoryService {}", versionHistoryService);
+        logger.error("beforeVersioning {}", versionHistoryService.GetVersionHistory(c.apix(testNode)));
         //No version in the beginning
         VersionHistory beforeVersioning = versionHistoryService.GetVersionHistory(c.apix(testNode));
         assertEquals(beforeVersioning.getVersionHistory().size(), 1); // changed from null to 1.
@@ -88,60 +89,15 @@ public class VersionHistoryServiceTestJavaApi extends JavaApiBaseTest {
         VersionHistory beforeVersioning = versionHistoryService.GetVersionHistory(c.apix(testNode));
         assertEquals(beforeVersioning.getVersionHistory().size(), 1);
 
-        //First a minor version
-        logger.error("TestCreateVersion - versionProperties.put( {}   , {})", VersionBaseModel.PROP_VERSION_TYPE, VersionType.MINOR );
         versionProperties.put(VersionBaseModel.PROP_VERSION_TYPE, VersionType.MINOR);
-        // Example of above to set the versionProps...
-        //        versionProperties.put(VersionBaseModel.PROP_VERSION_TYPE, org.alfresco.service.cmr.version.VersionType.MINOR);
-
-        // TODO - fails due to RO-mode (even with license uploaded)
-//         Error =06040043 Access Denied.  The system is currently in read-only mode.
-//        org.alfresco.service.transaction.ReadOnlyServerException: 06040043 Access Denied.  The system is currently in read-only mode.
-//        	at org.alfresco.repo.domain.node.AbstractNodeDAOImpl.getCurrentTransaction(AbstractNodeDAOImpl.java:586)
-//        	at org.alfresco.repo.domain.node.AbstractNodeDAOImpl.newNodeImpl(AbstractNodeDAOImpl.java:1309)
-//        	at org.alfresco.repo.domain.node.AbstractNodeDAOImpl.newNode(AbstractNodeDAOImpl.java:1233)
-//        	at org.alfresco.repo.node.db.DbNodeServiceImpl.createNode_aroundBody24(DbNodeServiceImpl.java:392)
-//        	at org.alfresco.repo.node.db.DbNodeServiceImpl$AjcClosure25.run(DbNodeServiceImpl.java:1)
-//        	at org.aspectj.runtime.reflect.JoinPointImpl.proceed(JoinPointImpl.java:179)
-//        	at org.alfresco.traitextender.RouteExtensions.intercept(RouteExtensions.java:100)
-//        	at org.alfresco.repo.node.db.DbNodeServiceImpl.createNode(DbNodeServiceImpl.java:342)
-//        	at jdk.internal.reflect.GeneratedMethodAccessor627.invoke(Unknown Source)
-//        	at java.base/jdk.internal.reflect.DelegatingMethodAccessorImpl.invoke(Unknown Source)
-//        	at java.base/java.lang.reflect.Method.invoke(Unknown Source)
-//        	at org.springframework.aop.support.AopUtils.invokeJoinpointUsingReflection(AopUtils.java:343)
-//        	at org.springframework.aop.framework.ReflectiveMethodInvocation.invokeJoinpoint(ReflectiveMethodInvocation.java:196)
-//        	at org.springframework.aop.framework.ReflectiveMethodInvocation.proceed(ReflectiveMethodInvocation.java:163)
-//        	at org.alfresco.repo.lock.mem.LockableAspectInterceptor.invoke(LockableAspectInterceptor.java:244)
-//        	at org.springframework.aop.framework.ReflectiveMethodInvocation.proceed(ReflectiveMethodInvocation.java:184)
-//        	at org.springframework.aop.framework.JdkDynamicAopProxy.invoke(JdkDynamicAopProxy.java:244)
-//        	at jdk.proxy3/jdk.proxy3.$Proxy46.createNode(Unknown Source)
-//        	at jdk.internal.reflect.GeneratedMethodAccessor627.invoke(Unknown Source)
-//        	at java.base/jdk.internal.reflect.DelegatingMethodAccessorImpl.invoke(Unknown Source)
-//        	at java.base/java.lang.reflect.Method.invoke(Unknown Source)
-//        	at org.springframework.aop.support.AopUtils.invokeJoinpointUsingReflection(AopUtils.java:343)
-//        	at org.springframework.aop.framework.ReflectiveMethodInvocation.invokeJoinpoint(ReflectiveMethodInvocation.java:196)
-//        	at org.springframework.aop.framework.ReflectiveMethodInvocation.proceed(ReflectiveMethodInvocation.java:163)
-//        	at org.alfresco.repo.tenant.MultiTNodeServiceInterceptor.invoke(MultiTNodeServiceInterceptor.java:111)
-//        	at org.springframework.aop.framework.ReflectiveMethodInvocation.proceed(ReflectiveMethodInvocation.java:184)
-//        	at org.springframework.aop.framework.JdkDynamicAopProxy.invoke(JdkDynamicAopProxy.java:244)
-//        	at jdk.proxy3/jdk.proxy3.$Proxy46.createNode(Unknown Source)
-//        	at org.alfresco.repo.version.Version2ServiceImpl.createNewVersion(Version2ServiceImpl.java:520)
-//        	at org.alfresco.repo.version.Version2ServiceImpl.createVersion(Version2ServiceImpl.java:324)
-//        	at org.alfresco.repo.version.Version2ServiceImpl.createVersion_aroundBody2(Version2ServiceImpl.java:158)
-//        	at org.alfresco.repo.version.Version2ServiceImpl$AjcClosure3.run(Version2ServiceImpl.java:1)
-//        	at org.aspectj.runtime.reflect.JoinPointImpl.proceed(JoinPointImpl.java:179)
-//        	at org.alfresco.traitextender.RouteExtensions.intercept(RouteExtensions.java:100)
-//        	at org.alfresco.repo.version.Version2ServiceImpl.createVersion(Version2ServiceImpl.java:142)
-//        	at eu.xenit.apix.alfresco.versionhistory.VersionHistoryService.createVersion(VersionHistoryService.java:83)
-//        	at eu.xenit.apix.tests.versionhistory.VersionHistoryServiceTest.TestCreateVersion(VersionHistoryServiceTest.java:116)
-        versionHistoryService.createVersion(c.apix(testNode), versionProperties);
-//        org.alfresco.service.cmr.version.Version version = alfrizcoVersionHistoryService
-//                        .createVersion(testNode, versionProperties);
+//        06150065 Access Denied.  The system is currently in read-only mode.
+//      org.alfresco.service.transaction.ReadOnlyServerException: 06150065 Access Denied.  The system is currently in read-only mode.
+//        versionHistoryService.createVersion(c.apix(testNode), versionProperties);
+        alfrizcoVersionHistoryService.createVersion(testNode, versionProperties);
+//        VersionHistory initialVersioning = versionHistoryService.GetVersionHistory(c.apix(testNode));
         VersionHistory initialVersioning = versionHistoryService.GetVersionHistory(c.apix(testNode));
-        logger.error("TestCreateVersion - initialVersioning( {}  )", initialVersioning);
-
         assertNotEquals(initialVersioning, null);
-        assertEquals(initialVersioning.getVersionHistory().size(), 2); // changed from 1 -> 2
+        assertEquals(2, initialVersioning.getVersionHistory().size()); // changed from 1 -> 2
         Version firstVersion = initialVersioning.getVersionHistory().get(0);
         assertNotEquals(firstVersion.getModifiedDate(), null);
         assertEquals(Version.VersionType.MINOR, firstVersion.getType());
