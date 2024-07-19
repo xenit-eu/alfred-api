97e109b6fab70bd292aa33607c24e602619c12d7
Benjamin Van der Smissen <benjamin.vandersmissen@xenit.eu>
2024-07-19 11:25:10 +0200

    ALFREDAPI-548 [Cleanup] Version to 6.0.0

---
+++ apix-integrationtests-client/alfresco/src/main/java/eu/xenit/apix/tests/workflow/methods/WorkflowService_ReleaseOnOtherClaimedTasksAsAdmin_Test.java
@@ -0,0 +1,57 @@
+package eu.xenit.apix.tests.workflow.methods;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.alfresco.repo.security.authentication.AuthenticationUtil;
+import org.alfresco.repo.transaction.RetryingTransactionHelper;
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WorkflowService_ReleaseOnOtherClaimedTasksAsAdmin_Test extends
+        WorkflowService_BaseMethod_Test {
+
+    protected final static Logger logger = LoggerFactory
+            .getLogger(WorkflowService_ReleaseOnOtherClaimedTasksAsAdmin_Test.class);
+
+    protected void setupSampleDataLocal() {
+        Object result = this.transactionHelper
+                .doInTransaction(new RetryingTransactionHelper.RetryingTransactionCallback<Object>() {
+                    @Override
+                    public Object execute() {
+                        tasksToMap.put("testReleaseOnOtherClaimedTasksAsAdmin", new ArrayList() {{
+                            add(createSampleWorkflowUserMethodMap(ACTIVITI_POOLED_REVIEW, USER_A, GROUP_USERS));
+                        }});
+                        return null;
+                    }
+                }, false, true);
+    }
+
+    @Test
+    public void testReleaseOnOtherClaimedTasksAsAdmin() {
+        String testName = new Object() {
+        }.getClass().getEnclosingMethod().getName();
+        logger.debug(" ===================================  TEST ==================================== ");
+        logger.debug(" " + testName);
+
+        AuthenticationUtil.setFullyAuthenticatedUser(USER_ADMIN);
+
+        List<String> taskIDs = this.tasks.get(new Object() {
+        }.getClass().getEnclosingMethod().getName());
+        for (String taskID : taskIDs) {
+            if (taskID == null) {
+                Assert.fail("Could not find pre-configured task for this test.");
+            }
+
+            assertFalse(apixProbeReleaseTask(taskID, USER_ADMIN));
+            AuthenticationUtil.setFullyAuthenticatedUser(USER_A);
+            assertTrue(apixProbeClaimTask(taskID, USER_A));
+            AuthenticationUtil.setFullyAuthenticatedUser(USER_ADMIN);
+            assertFalse(alfrescoProbeReleaseTask(taskID, USER_ADMIN));
+        }
+    }
+}
---
+++ apix-integrationtests-client/alfresco/src/main/java/eu/xenit/apix/tests/workflow/methods/WorkflowService_TransitionSomeoneElseTask_Test.java
@@ -0,0 +1,59 @@
+package eu.xenit.apix.tests.workflow.methods;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.alfresco.repo.security.authentication.AuthenticationUtil;
+import org.alfresco.repo.transaction.RetryingTransactionHelper;
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WorkflowService_TransitionSomeoneElseTask_Test extends WorkflowService_BaseMethod_Test {
+
+    protected final static Logger logger = LoggerFactory
+            .getLogger(WorkflowService_TransitionSomeoneElseTask_Test.class);
+
+    protected void setupSampleDataLocal() {
+        Object result = this.transactionHelper
+                .doInTransaction(new RetryingTransactionHelper.RetryingTransactionCallback<Object>() {
+                    @Override
+                    public Object execute() {
+                        tasksToMap.put("testTransitioningSomeoneElseTasks", new ArrayList() {{
+                            add(createSampleWorkflowUserMethodMap(ACTIVITI_POOLED_REVIEW, USER_A, GROUP_USERS));
+                        }});
+                        return null;
+                    }
+                }, false, true);
+    }
+
+    @Test(expected = Error.class)
+    public void testTransitioningSomeoneElseTasks() {
+        String testName = new Object() {
+        }.getClass().getEnclosingMethod().getName();
+        logger.debug(" ===================================  TEST ==================================== ");
+        logger.debug(" " + testName);
+        AuthenticationUtil.setFullyAuthenticatedUser(USER_B);
+
+        List<String> taskIDs = this.tasks.get(testName);
+        for (String taskId : taskIDs) {
+            if (taskId == null) {
+                Assert.fail("Could not find pre-configured task for this test.");
+            }
+
+            Map<String, List<String>> targetTasksMap = new HashMap<>();
+            initTransitionsAndTasks(testName, taskId, targetTasksMap);
+
+            for (String transition : targetTasksMap.keySet()) {
+                List<String> taskIdToTransitions = targetTasksMap.get(transition);
+                for (String taskIdToTransition : taskIdToTransitions) {
+                    logger.debug("Claimed task: '" + taskIdToTransition + "' " + apixProbeClaimTask(taskIdToTransition,
+                            USER_A));
+                    apixWorkflowService.endTask(taskId, transition);
+                }
+            }
+        }
+    }
+}
