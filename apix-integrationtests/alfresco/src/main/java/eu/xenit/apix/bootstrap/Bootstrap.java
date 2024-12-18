9262989f49057813e5de04ecc7fdcb548aa708d7
Robrecht Vanhuysse <robrecht.vanhuysse@xenit.eu>
2024-12-18 16:16:20 +0100

    Only enable integrationtest bootstrap class locally through alf global property

--- apix-integrationtests/alfresco/src/main/java/eu/xenit/apix/bootstrap/Bootstrap.java
+++ apix-integrationtests/alfresco/src/main/java/eu/xenit/apix/bootstrap/Bootstrap.java
@@ -3,6 +3,7 @@ package eu.xenit.apix.bootstrap;
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 import org.alfresco.model.ContentModel;
 import org.alfresco.repo.nodelocator.CompanyHomeNodeLocator;
 import org.alfresco.repo.nodelocator.NodeLocatorService;
@@ -14,52 +15,61 @@ import org.alfresco.service.namespace.NamespaceService;
 import org.alfresco.service.namespace.QName;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Bootstrap implements InitializingBean {
 
     public static final String WELL_KNOWN_TESTNODE_NAME = "well-known-testnode";
+    private static final String ENABLED_GLOBAL_PROPERTIES_KEY = "eu.xenit.apix.integrationtest.bootstrap.enabled";
 
     private RetryingTransactionHelper transactionHelper;
     private NodeLocatorService nodeLocator;
     private NodeService nodeService;
+    private Properties globalProperties;
 
     @Autowired
     public Bootstrap(
             RetryingTransactionHelper retryingTransactionHelper,
             NodeLocatorService nodeLocatorService,
-            NodeService nodeService
+            NodeService nodeService,
+            @Qualifier("global-properties") Properties globalProperties
             ) {
         this.transactionHelper = retryingTransactionHelper;
         this.nodeLocator = nodeLocatorService;
         this.nodeService = nodeService;
+        this.globalProperties = globalProperties;
     }
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        AuthenticationUtil.runAsSystem(
-                () -> transactionHelper.doInTransaction(
-                    () -> {
-                            NodeRef companyHome = nodeLocator.getNode(CompanyHomeNodeLocator.NAME, null, null);
-                            NodeRef wellKnownTestNode = nodeService.getChildByName(companyHome, ContentModel.ASSOC_CONTAINS, WELL_KNOWN_TESTNODE_NAME);
-                            if (wellKnownTestNode == null) {
-                                Map<QName, Serializable> folderProperties = new HashMap<>();
-                                folderProperties.put(ContentModel.PROP_NAME, WELL_KNOWN_TESTNODE_NAME);
-                                folderProperties.put(ContentModel.PROP_NODE_UUID, WELL_KNOWN_TESTNODE_NAME);
-                                nodeService.createNode(
-                                        companyHome,
-                                        ContentModel.ASSOC_CONTAINS,
-                                        QName.createQName(NamespaceService.CONTENT_MODEL_1_0_URI, WELL_KNOWN_TESTNODE_NAME),
-                                        ContentModel.TYPE_FOLDER,
-                                        folderProperties
-                                );
-                            }
-                            return null;
-                        },
-                false,
-                true
-            )
-        );
+        if (Boolean.parseBoolean(globalProperties.getProperty(ENABLED_GLOBAL_PROPERTIES_KEY))) {
+            AuthenticationUtil.runAsSystem(
+                    () -> transactionHelper.doInTransaction(
+                            () -> {
+                                NodeRef companyHome = nodeLocator.getNode(CompanyHomeNodeLocator.NAME, null, null);
+                                NodeRef wellKnownTestNode = nodeService.getChildByName(companyHome,
+                                        ContentModel.ASSOC_CONTAINS, WELL_KNOWN_TESTNODE_NAME);
+                                if (wellKnownTestNode == null) {
+                                    Map<QName, Serializable> folderProperties = new HashMap<>();
+                                    folderProperties.put(ContentModel.PROP_NAME, WELL_KNOWN_TESTNODE_NAME);
+                                    folderProperties.put(ContentModel.PROP_NODE_UUID, WELL_KNOWN_TESTNODE_NAME);
+                                    nodeService.createNode(
+                                            companyHome,
+                                            ContentModel.ASSOC_CONTAINS,
+                                            QName.createQName(NamespaceService.CONTENT_MODEL_1_0_URI,
+                                                    WELL_KNOWN_TESTNODE_NAME),
+                                            ContentModel.TYPE_FOLDER,
+                                            folderProperties
+                                    );
+                                }
+                                return null;
+                            },
+                            false,
+                            true
+                    )
+            );
+        }
     }
 }
